5c65e93fc06e5f5f5e5ed39ba7bfe6dd3bc605a8
Robert Deutz <rdeutz@googlemail.com>
2022-08-05 18:15:16 +0200

    WIP: convert to cypress

--- src/extensions.js
+++ src/extensions.js
@@ -1,263 +1,225 @@
 const extensionsCommands = () => {
 
-    /**
-     * Installs a Extension in Joomla that is located in a folder inside the server
-     *
-     * @param   String  $path  Path for the Extension
-     * @param   string  $type  Type of Extension
-     *
-     * {@internal doAdminLogin() before}
-     *
-     * @return    void
-     *
-     * @since    3.0.0
-     * @throws Exception
-     */
-    public function installExtensionFromFolder($path, $type = 'Extension')
-    {
-        $this->amOnPage('/administrator/index.php?option=com_installer');
-        $this->waitForText('Extensions: Install', '30', ['css' => 'H1']);
-        $this->click(['link' => 'Install from Folder']);
-        $this->debug('I enter the Path');
-        $this->fillField(['id' => 'install_directory'], $path);
-        $this->click(['id' => 'installbutton_directory']);
-        $this->waitForText('was successful', $this->config['timeout'], ['id' => 'system-message-container']);
-        $this->debug("$type successfully installed from $path");
+    // Installs a Extension in Joomla that is located in a folder inside the server
+    const installExtensionFromFolder = (path, type = 'Extension') => {
+        cy.log('**Install an extension from folder**')
+        cy.log('Path: ' + path)
+        cy.log('Type: ' + type)
+
+        cy.visit('/administrator/index.php?option=com_installer')
+
+        cy.contains('Install from Folder').click()
+
+        cy.get('#install_directory').fill(path)
+        cy.get('#installbutton_directory').click()
+
+        cy.get('#system-message-container').contains('was successful')
+
+        cy.log('--Install an extension from folder--')
     }
 
-    /**
-     * Installs a Extension in Joomla that is located in a url
-     *
-     * @param   String  $url   Url address to the .zip file
-     * @param   string  $type  Type of Extension
-     *
-     * {@internal doAdminLogin() before}
-     *
-     * @return    void
-     *
-     * @since    3.0.0
-     * @throws Exception
-     */
-    public function installExtensionFromUrl($url, $type = 'Extension')
-    {
-        $this->amOnPage('/administrator/index.php?option=com_installer');
-        $this->waitForText('Extensions: Install', '30', ['css' => 'H1']);
-        $this->wait(1);
-        $this->waitForElementNotVisible(['xpath' => '//joomla-core-loader']);
-        $this->click(['link' => 'Install from URL']);
-        $this->debug('I enter the url');
-        $this->fillField(['id' => 'install_url'], $url);
-        $this->click(['id' => 'installbutton_url']);
-        $this->waitForText('was successful', '30', ['id' => 'system-message-container']);
+    Cypress.Commands.add('installExtensionFromFolder', installExtensionFromFolder)
 
-        if ($type == 'Extension')
-        {
-            $this->debug('Extension successfully installed from ' . $url);
-        }
 
-        if ($type == 'Plugin')
-        {
-            $this->debug('Installing plugin was successful.' . $url);
-        }
+    // Installs a Extension in Joomla that is located in a url
+    const installExtensionFromUrl = (url, type = 'Extension') => {
+        cy.log('**Install an extension from Url**')
+        cy.log('Url: ' + url)
+        cy.log('Type: ' + type)
 
-        if ($type == 'Package')
-        {
-            $this->debug('Installation of the package was successful.' . $url);
-        }
+        cy.visit('/administrator/index.php?option=com_installer')
+
+        cy.contains('Install from URL').click()
+
+        cy.get('#install_url').fill(url)
+        cy.get('#installbutton_url').click()
+
+        cy.get('#system-message-container').contains('was successful')
+
+        cy.log('--Install an extension from Url--')
     }
 
-    /**
-     * Installs a Extension in Joomla using the file upload option
-     *
-     * @param   string  $file  Path to the file in the _data folder
-     * @param   string  $type  Type of Extension
-     *
-     * {@internal doAdminLogin() before}
-     *
-     * @return    void
-     * @throws Exception
-     */
-    public function installExtensionFromFileUpload($file, $type = 'Extension')
+    Cypress.Commands.add('installExtensionFromUrl', installExtensionFromUrl)
+
+
+    // Installs a Extension in Joomla using the file upload option
+    const installExtensionFromFileUpload = (file, type = 'Extension') =>
     {
-        $this->amOnPage('/administrator/index.php?option=com_installer');
-        $this->waitForText('Extensions: Install', '30', array('css' => 'H1'));
-        $this->click(array('link' => 'Upload Package File'));
+        cy.log('**Install an extension from file upload**')
+        cy.log('File: ' + file)
+        cy.log('Type: ' + type)
 
-        $this->debug('I make sure legacy uploader is visible');
-        $this->executeJS('document.getElementById("legacy-uploader").style.display="block";');
+        cy.visit('/administrator/index.php?option=com_installer')
 
-        $this->debug('I enter the file input');
-        $this->attachFile(array('id' => 'install_package'), $file);
+        cy.contains('Upload Package File').click()
 
-        $this->waitForText('was successful', '30', array('id' => 'system-message-container'));
+        document.getElementById('legacy-uploader').classList.remove('hidden')
 
-        if ($type == 'Extension')
-        {
-            $this->debug('Extension successfully installed.');
-        }
+        cy.get('#install_package').attachFile(file)
 
-        if ($type == 'Plugin')
-        {
-            $this->debug('Installing plugin was successful.');
-        }
+        cy.get('#system-message-container').contains('was successful')
 
-        if ($type == 'Package')
-        {
-            $this->debug('Installation of the package was successful.');
-        }
+        cy.log('--Install an extension from file upload--')
     }
 
-    /**
-     * Function to Enable a Plugin
-     *
-     * @param   String  $pluginName  Name of the Plugin
-     *
-     * @return  void
-     *
-     * @since   3.0.0
-     * @throws Exception
-     */
-    public function enablePlugin($pluginName)
+    Cypress.Commands.add('installExtensionFromFileUpload', installExtensionFromFileUpload)
+
+
+    // Function to Enable a Plugin
+    const enablePlugin = (pluginName) =>
     {
-        $this->amOnPage('/administrator/index.php?option=com_plugins');
-        $this->debug('I check for Notices and Warnings');
-        $this->checkForPhpNoticesOrWarnings();
-        $this->searchForItem($pluginName);
-        $this->waitForElement($this->searchResultPluginName($pluginName), 30);
-        $this->debug("Verifying if $pluginName exist in search result");
-        $this->seeElement(['xpath' => "//form[@id='adminForm']/div/table/tbody"]);
-        $this->see($pluginName, ['xpath' => "//form[@id='adminForm']/div/table/tbody"]);
-        $this->click(['xpath' => "//input[@id='cb0']"]);
-        $this->click(['xpath' => "//div[@id='toolbar-publish']/button"]);
-        $this->waitForText(' enabled', $this->config['timeout'], ['id' => 'system-message-container']);
+        cy.log('**Enable Plugin**')
+        cy.log('Plugin Name: ' + pluginName)
+
+        cy.visit('/administrator/index.php?option=com_plugins')
+
+        // TODO: Do we need this?
+        cy.checkForPhpNoticesOrWarnings()
+
+        cy.searchForItem(pluginName)
+
+        cy.get('#system-message-container .alert').should('not.exist')
+
+        cy.get('#cb0').click()
+        cy.get('#toolbar-publish button').click()
+        cy.get('#system-message-container').contains('enabeld')
+
+        cy.log('--Enable Plugin--')
     }
 
+    Cypress.Commands.add('enablePlugin', enablePlugin)
 
-    /**
-     * Uninstall Extension based on a name
-     *
-     * @param   string  $extensionName  Is important to use a specific
-     *
-     * @return  void
-     *
-     * @since   3.0.0
-     * @throws Exception
-     */
-    public function uninstallExtension($extensionName)
+
+    // Uninstall Extension based on a name
+    const uninstallExtension = (extensionName) =>
     {
-        $this->amOnPage('/administrator/index.php?option=com_installer&view=manage');
-        $this->waitForText('Extensions: Manage', '30', ['css' => 'H1']);
-        $this->searchForItem($extensionName);
-        $this->waitForElement(['id' => 'manageList'], '30');
-        $this->click(['xpath' => "//input[@id='cb0']"]);
-        $this->click(['xpath' => "//joomla-toolbar-button[@id='toolbar-delete']/button"]);
-        $this->acceptPopup();
-        $this->waitForText('was successful', '30', ['id' => 'system-message-container']);
-        $this->see('was successful', ['id' => 'system-message-container']);
-        $this->debug('I check for warnings during the uninstall process');
-        $this->dontSeeElement(['xpath' => "//joomla-alert[@type='warning']"]);
-        $this->searchForItem($extensionName);
-        $this->waitForText(
-            'No Matching Results',
-            $this->config['timeout'],
-            ['class' => 'alert-info']
-    );
-        $this->see('No Matching Results', ['class' => 'alert-info']);
-        $this->debug('Extension successfully uninstalled');
+        cy.log('**Uninstall an extension**')
+        cy.log('Extension Name: ' + extensionName)
+
+        cy.visit('/administrator/index.php?option=com_installer&view=manage')
+
+        cy.searchForItem(extensionName)
+
+        cy.get('#system-message-container .alert').should('not.exist')
+
+        cy.get('#cb0').click()
+        cy.get('#toolbar-delete button').click()
+        cy.get('#system-message-container').contains('was successful')
+
+        // Check for warnings during install
+        cy.get('joomla-alert[@type="warning"]').should('not.be.visible')
+
+        cy.searchForItem(extensionName)
+        cy.get('#system-message-container .alert').contains(' No Matching Results ').should('exist')
+
+        cy.log('--Uninstall an extension--')
     }
 
+    Cypress.Commands.add('uninstallExtension', uninstallExtension)
 
-
-    /**
-     * Function to install a language through the interface
-     *
-     * @param   string  $languageName  Name of the language you want to install
-     *
-     * @return void
-     * @throws Exception
-     */
-    public function installLanguage($languageName)
+    // Function to install a language through the interface
+    const installLanguage = (languageName) =>
     {
-        $this->amOnPage('administrator/index.php?option=com_installer&view=languages');
-        $this->debug('I check for Notices and Warnings');
-        $this->checkForPhpNoticesOrWarnings();
-        $this->debug('Refreshing languages');
-        $this->click(['xpath' => "//div[@id='toolbar-refresh']/button"]);
-        $this->waitForElement(['id' => 'j-main-container'], 30);
-        $this->searchForItem($languageName);
-        $this->waitForElement($this->searchResultLanguageName($languageName), 30);
-        $this->click(['id' => "cb0"]);
-        $this->click(['xpath' => "//div[@id='toolbar-upload']/button"]);
-        $this->waitForText('was successful.', $this->config['timeout'], ['id' => 'system-message-container']);
-        $this->see('No Matching Results', ['class' => 'alert-no-items']);
-        $this->debug($languageName . ' successfully installed');
+        cy.log('**Install a language**')
+        cy.log('Language Name: ' + languageName)
+
+        cy.visit('/administrator/index.php?option=com_installer&view=manage')
+
+        // TODO: Do we need this?
+        cy.checkForPhpNoticesOrWarnings()
+
+        cy.searchForItem(languageName)
+
+        cy.get('#system-message-container .alert').should('not.exist')
+
+        cy.get('tr.row0 input').click()
+        cy.get('#system-message-container').contains('was successful')
+
+        cy.log('--Install a language--')
     }
 
+    Cypress.Commands.add('installLanguage', installLanguage)
 
-    /**
-     * Publishes a module on frontend in given position
-     *
-     * @param   string  $module    The full name of the module
-     * @param   string  $position  The template position of a module. Right position by default
-     *
-     * @return void
-     * @throws Exception
-     */
-    public function setModulePosition($module, $position = 'position-7')
+
+    // Publishes a module on frontend in given position
+    const setModulePosition = (module, position = 'position-7') =>
     {
-        $this->amOnPage('administrator/index.php?option=com_modules');
-        $this->searchForItem($module);
-        $this->click(['link' => $module]);
-        $this->waitForText("Modules: $module", 30, ['css' => 'h1.page-title']);
-        $this->click(['link' => 'Module']);
-        $this->waitForElement(['id' => 'general'], 30);
-        $this->selectOptionInChosen('Position', $position);
-        $this->click(['xpath' => "//div[@id='save-group-children-apply']/button"]);
-        $this->waitForText('Module saved', 30, ['id' => 'system-message-container']);
+        cy.log('**Set module position**')
+        cy.log('Module: ' + module)
+        cy.log('Position: ' + position)
+
+        cy.visit('administrator/index.php?option=com_modules')
+
+        cy.searchForItem(module)
+
+        cy.get('#system-message-container .alert').should('not.exist')
+
+        cy.get('tr.row0 .has-context a').click()
+
+        // TODO: herausfinden wie ich das mit dem select mache
+        // $this->selectOptionInChosen('Position', $position);
+
+        cy.get('#toolbar-dropdown-save-group .button-save').click()
+        cy.get('#system-message-container').contains('saved')
+
+        cy.log('--Set module position--')
     }
 
-    /**
-     * Publishes a module on all frontend pages
-     *
-     * @param   string  $module  The full name of the module
-     *
-     * @return  void
-     *
-     * @since   3.0.0
-     * @throws Exception
-     */
-    public function publishModule($module)
+    Cypress.Commands.add('setModulePosition', setModulePosition)
+
+
+    // Publishes modules
+    const publishModule = (module) =>
     {
-        $this->amOnPage('administrator/index.php?option=com_modules');
-        $this->searchForItem($module);
-        $this->checkAllResults();
-        $this->click(['xpath' => "//div[@id='toolbar-publish']/button"]);
-        $this->waitForText(' published.', 30, ['id' => 'system-message-container']);
+        cy.log('**Publish all modules**')
+        cy.log('Module: ' + module)
+
+        cy.visit('administrator/index.php?option=com_modules')
+
+        cy.searchForItem(module)
+
+        cy.get('#system-message-container .alert').should('not.exist')
+
+        cy.get('#moduleList input[name="checkall-toggle"]').click()
+
+        // Make sure modules are unpublished
+        cy.get('#toolbar .button-unpublish').click()
+
+        cy.get('#toolbar .button-publish').click()
+
+        cy.get('#system-message-container').contains('published')
+
+        cy.log('--Publish all modules--')
     }
 
-    /**
-     * Changes the module Menu assignment to be shown on all the pages of the website
-     *
-     * @param   string  $module  The full name of the module
-     *
-     * @return  void
-     *
-     * @since   3.0.0
-     * @throws Exception
-     */
-    public function displayModuleOnAllPages($module)
+    Cypress.Commands.add('publishModule', publishModule)
+
+
+    // Changes the module Menu assignment to be shown on all the pages of the website
+    const displayModuleOnAllPages = (module) =>
     {
-        $this->amOnPage('administrator/index.php?option=com_modules');
-        $this->searchForItem($module);
-        $this->click(['link' => $module]);
-        $this->waitForElement(['link' => 'Menu Assignment'], 30);
-        $this->click(['link' => 'Menu Assignment']);
-        $this->waitForElement(['id' => 'jform_menus-lbl'], 30);
-        $this->click(['id' => 'jform_assignment_chzn']);
-        $this->click(['xpath' => "//li[@data-option-array-index='0']"]);
-        $this->click(['xpath' => "//div[@id='save-group-children-apply']/button"]);
-        $this->waitForText('Module saved', 30, ['id' => 'system-message-container']);
+        cy.log('**Display module on all pages**')
+        cy.log('Module: ' + module)
+
+        cy.visit('administrator/index.php?option=com_modules')
+
+        cy.searchForItem(module)
+
+        cy.get('#system-message-container .alert').should('not.exist')
+
+        cy.get('tr.row0 .has-context a').click()
+
+        cy.get('div[role="tablist"] button[aria-controls="assignment"]').click()
+
+        cy.get('#jform_assignment').select(0)
+
+        cy.get('#toolbar-dropdown-save-group .button-save').click()
+        cy.get('#system-message-container').contains('saved')
+
+        cy.log('--Display module on all pages--')
     }
+
+    Cypress.Commands.add('displayModuleOnAllPages', displayModuleOnAllPages)
 }
 
 module.exports = {
