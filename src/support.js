785368cc372390f00875d9970dd6c7cc5634e196
Sven Bluege <svenbluege@gmail.com>
2023-04-07 12:00:49 +0200

    Enable check for PHP errors and warnings.

--- src/support.js
+++ src/support.js
@@ -75,13 +75,13 @@ const supportCommands = () => {
   const checkForPhpNoticesOrWarnings = () => {
     cy.log('**Check for PHP notices and warnings**')
 
-    // cy.contains('Notice:').should('not.exists')
-    // cy.contains('<b>Notice</b>:').should('not.exists')
-    // cy.contains('Warning:').should('not.exists')
-    // cy.contains('<b>Warning</b>:').should('not.exists')
-    // cy.contains('Strict standards:').should('not.exists')
-    // cy.contains('<b>Strict standards</b>:').should('not.exists')
-    // cy.contains('The requested page can\'t be found').should('not.exists')
+    cy.contains('Notice:').should('not.exists')
+    cy.contains('<b>Notice</b>:').should('not.exists')
+    cy.contains('Warning:').should('not.exists')
+    cy.contains('<b>Warning</b>:').should('not.exists')
+    cy.contains('Strict standards:').should('not.exists')
+    cy.contains('<b>Strict standards</b>:').should('not.exists')
+    cy.contains('The requested page can\'t be found').should('not.exists')
 
     cy.log('--Check for PHP notices and warnings--')
   }
